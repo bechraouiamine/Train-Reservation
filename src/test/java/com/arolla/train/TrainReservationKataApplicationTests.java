7a3fd687619f6f47dab10f8b2e3a70ac92ef0ece
bechraoui amine <bechraoui.amine@gmail.com>
2021-02-10 14:35:10 +0100

    First Commit

---
+++ src/test/java/com/arolla/train/TrainReservationKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arolla.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainReservationKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
